313293a21c370b092f149838e5d7c3d3376cb067
Pauline <some@mail>
2025-09-05 10:52:11 +0300

    five methods

--- src/Main.java
+++ src/Main.java
@@ -8,17 +8,25 @@ public class Main {
         System.out.print("Введите второе число: ");
         int b = sc.nextInt();
 
-        System.out.println("Выберите, как будет проходить умножение: \n1 - множественное сложение\n2 - через логарифм\n3 - через рекурсию");
+        System.out.println("Выберите, как будет проходить умножение:\n1 - множественное сложение через цикл for" +
+                "\n2 - через логарифм\n3 - через рекурсию\n4 - через сложение в цикле while" +
+                "\n5 - через сдвиговую операцию");
         int what = sc.nextInt();
         switch (what){
             case 1:
-                MultiSum(a, b);
+                multiSumFor(a, b);
                 break;
             case 2:
-                MultiLog(a, b);
+                multiLog(a, b);
                 break;
             case 3:
-                System.out.println("Ответ: "+MultiRecurs(a, b));
+                System.out.println("Ответ: "+multiRecurs(a, b));
+                break;
+            case 4:
+                multiSumWhile(a, b);
+                break;
+            case 5:
+                multiPower(a, b);
                 break;
             default:
                 System.out.println("Такого варианта в списке нет");
@@ -27,7 +35,7 @@ public class Main {
 
     }
 
-    public static void MultiSum(int a, int b){
+    public static void multiSumFor(int a, int b){
         int res = 0;
         for(int i = 1; i<=a; i++){
             res+=b;
@@ -35,7 +43,7 @@ public class Main {
         System.out.println("Ответ: "+res);
     }
 
-    public static void MultiLog(int a, int b){
+    public static void multiLog(int a, int b){
         double aLog = Math.log10(a);
         double bLog = Math.log10(b);
         double c = aLog+bLog;
@@ -43,14 +51,14 @@ public class Main {
         System.out.println("Ответ: "+res);
     }
 
-    public static int MulRec(int summand, int steps_count){
+    public static int mulRec(int summand, int steps_count){
         if(steps_count>0){
-            return summand + MultiRecurs(summand, steps_count-1);
+            return summand + multiRecurs(summand, steps_count-1);
         }else{
             return 0;
         }
     }
-    public static int MultiRecurs(int a, int b){
+    public static int multiRecurs(int a, int b){
         int summand;
         int steps_count;
         Boolean negative;
@@ -63,8 +71,34 @@ public class Main {
             steps_count=Math.abs(a);
             negative = (a<0);
         }
-        int sum=MulRec(summand, steps_count);
+        int sum=mulRec(summand, steps_count);
         return negative ? -sum : sum;
+    }
+
+    public static void multiSumWhile(int a, int b){
+        int sum = 0;
+        while(b>0){
+            sum+=a;
+            b--;
+        }
+        System.out.println("Ответ: "+sum);
+    }
+
+    public static boolean isPowerTwo(int n){
+        return n>0 && (n & (n-1))==0;
+    }
+
+    public static void multiPower(int a, int b){
+        int ost = 0;
+        while(!isPowerTwo(b)){
+            ost++;
+            b--;
+        }
+        int res = a << (int)(Math.log(b)/Math.log(2));
+        for(int i = 1; i<=ost; i++){
+            res+=a;
+        }
+        System.out.println("Ответ: "+res);
     }
 
 }
